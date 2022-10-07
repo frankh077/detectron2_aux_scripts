c854903df431b34a9d7572dd82d8d4a11d9df287
Melissa <melquinonez9@gmail.com>
2022-10-07 15:53:20 -0500

    Add new scripts to different folders

---
+++ Script to Train/train.py
@@ -0,0 +1,96 @@
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os
+import json
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.structures import BoxMode
+
+
+folder_name='dataset'
+
+def get_dataset_dicts(directory):
+    classes = ['disease']
+    dataset_dicts = []
+    for idx, filename in enumerate([file for file in os.listdir(directory) if file.endswith('.json')]):
+        #print(f'idx: {idx}, filename: {filename}')
+        json_file = os.path.join(directory, filename)
+        with open(json_file) as f:
+            img_anns = json.load(f)
+
+        record = {}
+        print(f'Value: {img_anns["imagePath"]}')
+        filename = os.path.join(directory, img_anns["imagePath"])
+        
+        record["file_name"] = filename
+        #print(f'idx: {idx}, filename: {filename}')
+        record["image_id"] = idx
+        record["height"] = 3456
+        record["width"] = 4608
+      
+        annos = img_anns["shapes"]
+        objs = []
+        for anno in annos:
+            px = [a[0] for a in anno['points']]
+            py = [a[1] for a in anno['points']]
+            poly = [(x, y) for x, y in zip(px, py)]
+            poly = [p for x in poly for p in x]
+
+            obj = {
+                "bbox": [np.min(px), np.min(py), np.max(px), np.max(py)],
+                "bbox_mode": BoxMode.XYXY_ABS,
+                "segmentation": [poly],
+                "category_id": classes.index(anno['label']),
+                "iscrowd": 0
+            }
+            objs.append(obj)
+        record["annotations"] = objs
+        dataset_dicts.append(record)
+    print(f'{dataset_dicts}')
+    return dataset_dicts
+
+def get_trainer_config():
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.DATASETS.TRAIN = ("dataset_train",)
+    cfg.DATASETS.TEST = ()
+    cfg.DATALOADER.NUM_WORKERS = 4
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    cfg.SOLVER.IMS_PER_BATCH = 2
+    cfg.SOLVER.BASE_LR = 0.001
+    cfg.SOLVER.MAX_ITER = 100
+    #cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+    cfg.SOLVER.STEPS = []        # do not decay learning rate
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+    return cfg
+
+def main():
+    #Load dataset
+    #dicts = get_dataset_dicts(folder_name+'/')
+    for d in ["train", "test"]: 
+        DatasetCatalog.register("dataset_" + d, 
+                                lambda d=d: get_dataset_dicts(folder_name+'/' + d))
+        MetadataCatalog.get("dataset_" + d).set(thing_classes=['disease'])
+    #Setup trainer
+    custom_cfg = get_trainer_config()
+    #Instantiate trainer
+    trainer = DefaultTrainer(custom_cfg) 
+    trainer.resume_or_load(resume=False)
+    #Train
+    trainer.train()
+main()
